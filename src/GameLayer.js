209f0c742ef09a9904b7957e6c27d571ba0bca7e
Benyapa <ye.amity@gmail.com>
2016-03-29 10:14:07 +0700

    one way

--- src/GameLayer.js
+++ src/GameLayer.js
@@ -1,12 +1,43 @@
 var GameLayer = cc.LayerColor.extend({
     init: function() {
-        this._super( new cc.Color( 127, 127, 127, 255 ) );
+        this._super( new cc.Color( 61, 177, 255, 255 ) );
         this.setPosition( new cc.Point( 0, 0 ) );
         
-        var player = new Player();
-        player.setPosition( new cc.Point( 200, 200 ) );
-        this.addChild( player );
+        this.player = new Player();
+        this.player.setPosition( new cc.Point( 550, 50 ) );
+        this.addChild( this.player );
+        this.player.scheduleUpdate();
+        
+        this.way = new Way();
+        this.way.setPosition( new cc.Point( 300, 50 ) );
+        this.addChild( this.way );
+        this.way.scheduleUpdate();
+//        
+//        this.bg = new BG();
+//        this.bg.setPosition( new cc.Point( 200, 200 ) );
+//        this.addChild( this.bg );
+//        this.bg.scheduleUpdate();
+        
         return true;
+    },
+     addKeyboardHandlers: function() {
+        var self = this;
+        cc.eventManager.addListener({
+            event: cc.EventListener.KEYBOARD,
+            onKeyPressed : function( keyCode, event ) {
+                self.onKeyDown( keyCode, event );
+            },
+            onKeyReleased: function( keyCode, event ) {
+                self.onKeyUp( keyCode, event );
+            }
+        }, this);
+    },
+    
+    onKeyDown: function( keyCode, event ) {
+        
+    },
+ 
+    onKeyUp: function( keyCode, event ) {
     }
 });
  
--- src/Player.js
+++ src/Player.js
@@ -1,6 +1,12 @@
-var Cat = cc.Sprite.extend({
+var Player = cc.Sprite.extend({
     ctor: function() {
         this._super();
         this.initWithFile( 'res/images/Cat.png' );
+    },
+
+    update: function( dt ) {
+	    var pos = this.getPosition();
+	    this.setPosition( new cc.Point( pos.x, pos.y + 5 ) );
     }
+    
 });
